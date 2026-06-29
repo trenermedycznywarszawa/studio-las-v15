349a559d87fe07effeecb212a997fc4f955fd326
trenermedycznywarszawa <trenermedycznywarszawa@gmail.com>
2026-06-29 17:43:26 +0200

    Add production Supabase config bootstrap

---
+++ studio-las-config.js
@@ -0,0 +1,9 @@
+(function () {
+  window.STUDIO_LAS_CONFIG = Object.freeze({
+    supabase: Object.freeze({
+      projectRef: "ufcumhbnuyernuwepcij",
+      url: "https://ufcumhbnuyernuwepcij.supabase.co",
+      publishableKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmY3VtaGJudXllcm51d2VwY2lqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxOTgyOTUsImV4cCI6MjA5Nzc3NDI5NX0.slaL2PyVJqeMy67R1Hk4hAT1JJxPTJSlHTi3eXdWRCg"
+    })
+  });
+})();
